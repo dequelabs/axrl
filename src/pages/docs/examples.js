7fff6bcedd4cb1adf68897dd8a3e0bb1d8e589a9
Wilco Fiers <wilco.fiers@deque.com>
2019-02-10 18:09:57 +0100

    feat: Improve example page

--- src/pages/docs/examples.js
+++ src/pages/docs/examples.js
@@ -12,8 +12,35 @@ class IndexPage extends React.Component {
     return (
       <Layout title="AXRL Examples">
         <h1>AXRL Examples</h1>
+        <ul>
+          <li>
+            <a href="#simpleWebExample">
+              Simple web page test
+            </a>
+          </li>
+          <li>
+            <a href="#webPageTestExample1">
+              Complete test for a web page
+            </a>
+          </li>
+          <li>
+            <a href="#HTMLPageExample">
+              Multiple test for a web page
+            </a>
+          </li>
+          <li>
+            <a href="#simpleIOSExample">
+              Simple iOS UIViewController test
+            </a>
+          </li>
+          <li>
+            <a href="#simpleAndroidExample">
+              Simple Android Layout test
+            </a>
+          </li>
+        </ul>
 
-        <h2>Example: A basic web page test</h2>
+        <h2 id="simpleWebExample">Simple web page test</h2>
         <p>
           This example shows the test for example.com, where
           the test produced 2 results. One of the results is
@@ -21,14 +48,18 @@ class IndexPage extends React.Component {
         </p>
         <PrismJSON code={simpleWebExample} />
 
-        <h2>Example: Single test for a web page</h2>
+        <h2 id="webPageTestExample1">
+          Complete test for a web page
+        </h2>
         <p>
           The test of a web page with showcasing most of its
           properties
         </p>
         <PrismJSON code={webPageTestExample1} />
 
-        <h2>Example: Multiple test for a web page</h2>
+        <h2 id="HTMLPageExample">
+          Multiple test for a web page
+        </h2>
         <p>
           A single web page being tested multiple times.
           This can be used when using DOM Observer to track
@@ -37,17 +68,21 @@ class IndexPage extends React.Component {
         </p>
         <PrismJSON code={HTMLPageExample} />
 
-        <h2>Example: A basic iOS UIViewController test</h2>
+        <h2 id="simpleIOSExample">
+          Simple iOS UIViewController test
+        </h2>
         <p>
           An example similar to the simple web example,
           written for iOS.
         </p>
         <PrismJSON code={simpleIOSExample} />
 
-        <h2>Example: A basic Android Layout test</h2>
+        <h2 id="simpleAndroidExample">
+          Simple Android Layout test
+        </h2>
         <p>
           An example similar to the simple web example,
-          written for Android.
+          written for iOS.
         </p>
         <PrismJSON code={simpleAndroidExample} />
       </Layout>
